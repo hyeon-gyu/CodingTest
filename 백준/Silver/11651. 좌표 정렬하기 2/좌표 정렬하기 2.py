9577e4c32495ef7f4ba6e67739b21990065fb32b
hyeongyu lim <54972659+hyeon-gyu@users.noreply.github.com>
2023-06-19 20:11:17 +0900

    Update 좌표 정렬하기 2.py

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -10,3 +10,17 @@ for i in range(N):
 arr.sort(key = lambda x:(x[1], x[0]))
 for i in range(N):
     print(arr[i][0], arr[i][1])
+
+
+
+#두번째 방법(y,x 좌표를 바꿔서 생각하고 정렬하기)
+N = int(input())
+arr = []
+for i in range(N):
+    [x,y] = map(int,input().split())
+    arr.append([y,x])
+    
+arr.sort()
+for i in range(N):
+    print(arr[i][1], arr[i][0])
+    
