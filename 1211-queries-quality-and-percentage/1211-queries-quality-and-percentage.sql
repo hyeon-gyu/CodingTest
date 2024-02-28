194c7062613b60cdf863d9fcde1dd20ecd46d5eb
hyeongyu lim <54972659+hyeon-gyu@users.noreply.github.com>
2024-02-28 14:50:04 +0900

    Time: 723 ms (41.46%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,6 +1,15 @@
-select query_name ,
-round(sum(rating /position )/count(*),2) AS quality ,
-round(sum(CASE WHEN rating < 3 then 1 else 0 end)/count(*)*100,2) AS poor_query_percentage 
-from queries 
-where query_name IS NOT NULL
-group by query_name;
+# 방법1
+# select query_name ,
+# round(sum(rating /position )/count(*),2) AS quality ,
+# round(sum(CASE WHEN rating < 3 then 1 else 0 end)/count(*)*100,2) AS poor_query_percentage 
+# from queries 
+# where query_name IS NOT NULL
+# group by query_name;
+
+# 방법2
+select query_name, 
+    round(avg(rating/position),2) as quality, 
+    round(avg(if(rating < 3, 1, 0)) * 100,2) as poor_query_percentage
+from Queries 
+where query_name is not null
+group by query_name
