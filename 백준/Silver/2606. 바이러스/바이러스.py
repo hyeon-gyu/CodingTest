a0c7a0a7ab7ce09d1808f83fadc4335b874c6ea9
hyeongyu lim <54972659+hyeon-gyu@users.noreply.github.com>
2024-03-31 16:47:27 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,33 +1,24 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-def bfs(graph, visited, node):
-    count = 0
-    queue = deque([node])
-    visited[node] = True
-    while queue:
-        v = queue.popleft() 
-        #print(v, end = ' ')
-        for i in range(1,N+1):
-            if not visited[i] and graph[v][i]:
-                queue.append(i)
-                count += 1
-                visited[i] = True
-    return count            
-        
-N = int(input()) # N : 컴퓨터 수
-M = int(input()) # M : 연결되어있는 컴퓨터 쌍 수
+n = int(input())
+m = int(input())
+graph = [ [] for _ in range(n+1)]
+count = 0 
+visited = [ False for _ in range(n+1)  ]
 
-graph = [ [False] * (N + 1) for _ in range(N + 1)] # 그래프 초기화 0번부터 7번까지 (8행 8열)
+for i in range(m):
+    s, e = map(int, input().split())
+    graph[s].append(e) 
+    graph[e].append(s)
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
+def dfs(graph, v, visited):
+    visited[v] = True
+    global count
+    count += 1
+    for i in graph[v]:
+        if not visited[i] :
+            dfs(graph, i, visited)      
     
-    
-visited = [False] * (N+1) # 방문해야하는 컴퓨터 수
-node = 1 # 시작 번호
-
-print(bfs(graph,visited,node))
+dfs(graph, 1, visited)    
+print(count-1)
