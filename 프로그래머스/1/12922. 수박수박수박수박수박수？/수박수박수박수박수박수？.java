f1463c5945e858aa38238b775da32c68aa06383f
Hyeongyu Lim <54972659+hyeon-gyu@users.noreply.github.com>
2024-06-24 14:32:29 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.10 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int n) {
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i< n; i++){
+            if (i %2 == 0){
+                sb.append("수");
+            }
+            else{
+                sb.append("박");
+            }
+        }
+        return sb.toString();
+    }
+}
