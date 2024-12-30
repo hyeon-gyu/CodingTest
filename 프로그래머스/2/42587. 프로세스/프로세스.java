f89ab1c3e13b157fd88e94501b10f81246db2d36
Hyeongyu Lim <54972659+hyeon-gyu@users.noreply.github.com>
2024-12-30 22:52:39 +0900

    [level 2] Title: 프로세스, Time: 3.52 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int priority : priorities) {
+            pq.offer(priority);
+        }
+        while(!pq.isEmpty()) {
+            for(int i = 0; i < priorities.length; i++) {
+                if(priorities[i] == pq.peek()) {
+                    answer++;
+                    pq.poll();
+                    if(i == location) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
